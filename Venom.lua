befad67fce4461e053c29f1f00194ef8058284d9
V-abbas <96988693+V-abbas@users.noreply.github.com>
2023-09-02 15:23:36 +0300

    Update Venom.lua

--- Venom.lua
+++ Venom.lua
@@ -5644,7 +5644,7 @@ end
 end
 
 if text == 'تفعيل النداء التلقائي' or TextMsg == 'تفعيل لتاك التلقائي' then
-if redis:get(Venom..":settings:tagGr"..msg.chat_id) then
+if Redis:get(Venom..":settings:tagGr"..msg.chat_id) then
 Text = Reply_Status(msg.sender_id.user_id,"*⌔ : تم "..text.." بنجاح .*").by
 Redis:del(Venom..":settings:tagGr"..msg.chat_id)
 else
