9c9659562ad03c94055c7a492d16cd556a6ba525
V-abbas <96988693+V-abbas@users.noreply.github.com>
2025-07-21 01:00:22 +0300

    Update Venom.lua

--- Venom.lua
+++ Venom.lua
@@ -9802,25 +9802,33 @@ return LuaTele.sendText(msg_chat_id,msg_id,'⌔︙ اهلا بك عزيزي ال
 end
 end
 if text == 'قسم الاذاعه ⌔' then
-if not msg.ControllerBot then 
-return LuaTele.sendText(msg_chat_id,msg_id,'\n*⌔︙هاذا الامر يخص { '..Controller_Num(1)..' }* ',"md",true)  
-end
-local reply_markup = LuaTele.replyMarkup{type = 'keyboard',resize = true,is_personal = true,
-data = {
-{
-{text = 'اذاعه للمجموعات ⌔',type = 'text'},{text = 'اذاعه خاص ⌔', type = 'text'},
-},
-{
-{text = 'اذاعه بالتوجيه ⌔',type = 'text'},{text = 'اذاعه بالتوجيه خاص ⌔', type = 'text'},
-},
-{
-{text = 'اذاعه بالتثبيت ⌔',type = 'text'},
-},
-{
-{text = 'الغاء الامر ⌔',type = 'text'},
-},
-}
-}
+  if not msg.ControllerBot then 
+    return LuaTele.sendText(msg_chat_id, msg_id, '\n*⌔︙هاذا الامر يخص { '..Controller_Num(1)..' }* ', "md", true)  
+  end
+
+  local reply_markup = LuaTele.replyMarkup{
+    type = 'keyboard',
+    resize = true,
+    is_personal = true,
+    data = {
+      {
+        {text = 'اذاعه للمجموعات ⌔', type = 'text'},
+        {text = 'اذاعه خاص ⌔', type = 'text'},
+      },
+      {
+        {text = 'اذاعه بالتوجيه ⌔', type = 'text'},
+        {text = 'اذاعه بالتوجيه خاص ⌔', type = 'text'},
+      },
+      {
+        {text = 'اذاعه بالتثبيت ⌔', type = 'text'},
+      },
+      {
+        {text = 'الغاء الامر ⌔', type = 'text'},
+      },
+    }
+  }
+
+  return LuaTele.sendText(msg_chat_id, msg_id, "*⌔︙اختر نوع الاذاعه التي تريد تنفيذها*", "md", false, false, false, false, reply_markup)
 end
 if text == 'تنظيف المشتركين ⌔' then
 if not msg.ControllerBot then 
